9cc0fc9249f5cff594c3d1541b1afe371f121801
Phonixoop <33152793+Phonixoop@users.noreply.github.com>
2022-07-23 20:46:09 +0430

    console log added , get api added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const router = express.Router();
+const PORT = process.env.PORT || 3001;
 app.post('/feedback', (req, res) => {
     res.json(JSON.stringify(req.body));
 })
 
-app.listen(3001)
+console.log(`app is listening on port ${PORT}`);
+app.listen(PORT)
