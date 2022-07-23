faecf699896a439b38701a06d720e88bccbc4a2a
Phonixoop <33152793+Phonixoop@users.noreply.github.com>
2022-07-23 21:14:44 +0430

    multer added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const app = express();
-const router = express.Router();
+const multer = require('multer');
+const router = express.Router('/');
 const PORT = process.env.PORT || 3001;
+
 let bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+})
+
 app.get('/feedback', (req, res) => {
     res.status(200).send('it is working... 🤗');
 })
 
-app.post('/feedback', (req, res) => {
-    res.status(200).send('work again ' + JSON.stringify(req.body));
+app.post('/feedback', multer().none(), (req, res) => {
+    res.status(200).send('work again ' + req.body.name);
 })
 
 console.log(`app is listening on port ${PORT}`);
 app.listen(PORT)
+
+module.exports = router
